647b6776931c22b9461e967ddf1f9b94e8b90cca
entamago <81839879+entamago@users.noreply.github.com>
2021-06-21 21:17:49 +0900

    出品機能への遷移とActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :info
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :sales_status_id
+      validates :fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :fee_status
+  belongs_to :sales_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
