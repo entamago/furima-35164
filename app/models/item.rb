c7a0d7259bf99ddea25097062438ead04558db46
entamago <81839879+entamago@users.noreply.github.com>
2021-06-22 23:23:20 +0900

    ActiveStorageのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
   end
 
   has_one_attached :image
+  validate :image_presence
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -20,4 +21,15 @@ class Item < ApplicationRecord
   belongs_to :sales_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  private
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを選択してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 end
