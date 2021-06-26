65317af2b4f92afe34092faf81071492aff8396c
entamago <81839879+entamago@users.noreply.github.com>
2021-06-26 12:38:02 +0900

    ファクトリーボットの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                    {Faker::Lorem.word}
+    price                   {Faker::Number.between(from: 300, to: 9999999)}
+    info                    {Faker::Lorem.sentence}
+    category_id             {Faker::Number.between(from: 1, to: 10)}
+    sales_status_id         {Faker::Number.between(from: 1, to: 6)}
+    fee_status_id           {Faker::Number.between(from: 1, to: 2)}
+    prefecture_id           {Faker::Number.between(from: 1, to: 47)}
+    scheduled_delivery_id   {Faker::Number.between(from: 1, to: 3)}
+    association :user
   end
 end
