decd5238d2007409ba20dfd9b0edd7bca7a08734
entamago <81839879+entamago@users.noreply.github.com>
2021-07-10 18:17:14 +0900

    購入画面へのパスの記述と商品の表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,7 +1,15 @@
 class BuysController < ApplicationController
-  def new
+  def index
+    @item = Item.find(params[:item_id])
+    @buy = Buy.new
   end
 
   def create
+    @buy = Buy.new(buy_params)
+  end
+
+  private
+  def buy_params
+    params.require(:buy)
   end
 end
